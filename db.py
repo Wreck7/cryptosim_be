ccdf47ae2672a51f46bd3453103fb79b21cf26dc
Vishwa <vishwagovula07@gmail.com>
2025-11-14 20:44:28 +0530

    done working perfectly!

--- db.py
+++ db.py
@@ -8,3 +8,4 @@ url = 'https://yfyxmprbbtcmkzqxhsgp.supabase.co' # test database
 key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlmeXhtcHJiYnRjbWt6cXhoc2dwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEzOTY5NzcsImV4cCI6MjA2Njk3Mjk3N30.pgOhI9vJ4RegmJxitcvewYrHfuXdOGxLkPDFob069Rg'
 
 db = create_client(url, key)
+# key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlmeXhtcHJiYnRjbWt6cXhoc2dwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEzOTY5NzcsImV4cCI6MjA2Njk3Mjk3N30.pgOhI9vJ4RegmJxitcvewYrHfuXdOGxLkPDFob069Rg'
